d0c1b6e764eb559de4ea76ef5b0a3e51e2f49d30
Artem <artemev.art3m@yandex.ru>
2024-07-30 13:50:41 +0700

    Выполнена структуризация кода

---
+++ sandbox/src/main/java/ru/stqa/geometry/Geometry.java
@@ -0,0 +1,13 @@
+package ru.stqa.geometry;
+
+import ru.stqa.geometry.figures.Rectangle;
+import ru.stqa.geometry.figures.Square;
+
+public class Geometry {
+    public static void main(String[] args) {
+        Square.printSquareArea(5.0);
+
+        Rectangle.printRectangleArea(4.0, 6.0);
+    }
+
+}
