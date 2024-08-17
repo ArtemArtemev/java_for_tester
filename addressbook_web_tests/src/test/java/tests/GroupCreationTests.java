72011a057a235dda1da7e57892490078a772798e
Artem <artemev.art3m@yandex.ru>
2024-08-17 11:56:26 +0700

    Создан класс HelperBase

--- addressbook_web_tests/src/test/java/tests/GroupCreationTests.java
+++ addressbook_web_tests/src/test/java/tests/GroupCreationTests.java
@@ -6,18 +6,17 @@ import org.junit.jupiter.api.Test;
 public class GroupCreationTests extends TestBase {
 
     @Test
-    public void CanCreateGroup() {
-
+    public void canCreateGroup() {
         app.groups().createGroup(new GroupData("group name", "group futter", "group comment"));
     }
 
     @Test
-    public void CanCreateWithEmptyGroup() {
+    public void canCreateWithEmptyGroup() {
         app.groups().createGroup(new GroupData());
     }
 
     @Test
-    public void CanCreateWithEmptyNameOnly() {
+    public void canCreateWithEmptyNameOnly() {
         app.groups().createGroup(new GroupData().withName("some name"));
     }
 }
