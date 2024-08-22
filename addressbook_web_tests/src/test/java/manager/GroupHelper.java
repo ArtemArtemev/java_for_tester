6177d5292dd050d36458a91cf15f70b4345047b5
Artem <artemev.art3m@yandex.ru>
2024-08-22 22:17:38 +0700

    Добавлены циклы

--- addressbook_web_tests/src/test/java/manager/GroupHelper.java
+++ addressbook_web_tests/src/test/java/manager/GroupHelper.java
@@ -21,7 +21,7 @@ public class GroupHelper extends HelperBase {
     public void removeGroup() {
         openGroupsPage();
         selectGroup();
-        removeSelectedGroup();
+        removeSelectedGroups();
         returnToGroupsPage();
     }
 
@@ -41,12 +41,6 @@ public class GroupHelper extends HelperBase {
         }
     }
 
-    public boolean isGroupPresent() {
-        openGroupsPage();
-        return manager.isElementPresent(By.name("selected[]"));
-    }
-
-
     private void submitGroupCreation() {
         click(By.name("submit"));
     }
@@ -56,7 +50,7 @@ public class GroupHelper extends HelperBase {
     }
 
 
-    private void removeSelectedGroup() {
+    private void removeSelectedGroups() {
         click(By.name("delete"));
     }
 
@@ -82,4 +76,21 @@ public class GroupHelper extends HelperBase {
         click(By.name("selected[]"));
     }
 
+    public int getCount() {
+        openGroupsPage();
+        return manager.driver.findElements(By.name("selected[]")).size();
+    }
+
+    public void removeAllGroups() {
+        openGroupsPage();
+        selectAllGroups();
+        removeSelectedGroups();
+    }
+
+    private void selectAllGroups() {
+        var checkboxes = manager.driver.findElements(By.name("selected[]"));
+        for (var checkbox : checkboxes) {
+           checkbox.click();
+        }
+    }
 }
--- addressbook_web_tests/src/test/java/tests/GroupModificationTest.java
+++ addressbook_web_tests/src/test/java/tests/GroupModificationTest.java
@@ -7,7 +7,7 @@ public class GroupModificationTest extends TestBase{
 
     @Test
     void canModifyGroup() {
-        if(app.groups().isGroupPresent()){
+        if(app.groups().getCount() == 0){
             app.groups().createGroup(new GroupData("group name", "group futter", "group comment"));
         }
         app.groups().modifyGroup(new GroupData().withName("some name"));
